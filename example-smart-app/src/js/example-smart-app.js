2d6399a1d9fd1ca8c75b1b17b129779d9c27fca1
MedicalCopilotSmartApp <62674567+MedicalCopilotSmartApp@users.noreply.github.com>
2020-03-25 17:19:57 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -16,7 +16,8 @@
 
         $.when(pt).done(function(patient) {
           var mrn = patient.identifier.filter(function (x) { return x.type.text.toUpperCase() === 'MRN'; })[0].value;
-          window.location = 'https://idp-dev.healthviewanalytics.com/account/login?returnUrl=https%3A%2F%2Fembeddedanalytics-dev.healthviewanalytics.com%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=usercernersmartappoauth2';
+          window.location = 'https://localhost:44379/account/login?returnUrl=http%3A%2F%2Flocalhost%3A3000%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=test1cernersmartappoauth2';
+          
         });
       } else {
         onError();
