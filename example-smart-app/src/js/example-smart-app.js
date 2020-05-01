34c9727268876498d5105980ba197d9673236c44
MedicalCopilotSmartApp <62674567+MedicalCopilotSmartApp@users.noreply.github.com>
2020-04-30 20:41:15 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,18 +17,20 @@
         $.when(pt).done(function(patient) {
           var mrn = patient.identifier.filter(function (x) { return x.type.text.toUpperCase() === 'MRN'; })[0].value;
           
-          if (clientId === '04700180-5ff2-4eeb-963e-15fd859ba994') {
-            window.location = 'https://idp-dev.healthviewanalytics.com/account/login?returnUrl=https%3A%2F%2Fembeddedanalytics-dev.healthviewanalytics.com%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=usercernersmartappoauth2';  
-          }
-          else if (clientId === 'f08c6f2c-1469-4805-98b0-0e2c14718fa1') {
-            window.location = 'https://localhost:44379/account/login?returnUrl=http%3A%2F%2Flocalhost%3A3000%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=test1cernersmartappoauth2';
-          }
-          else if (clientId === '4ed79774-438b-4c40-9a9d-7df67e2f92d0') {
-            window.location = 'https://medicalcopilot.dev.lumedx.com/medicalcopilotidp/account/login?returnUrl=https%3A%2F%2Fmedicalcopilot.dev.lumedx.com%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=testcernersmartappoauth2';  
-          }
-          else if (clientId === 'ce726c87-2d1d-4824-9c4c-5e5598492a47') {
-            window.location = 'https://idp-test.healthviewanalytics.com/account/login?returnUrl=https%3A%2F%2Fembeddedanalytics-test.healthviewanalytics.com%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=cernersmartappoauth2';  
-          }
+          switch(clientId) {
+            case '04700180-5ff2-4eeb-963e-15fd859ba994':
+              window.location = 'https://idp-dev.healthviewanalytics.com/account/login?returnUrl=https%3A%2F%2Fembeddedanalytics-dev.healthviewanalytics.com%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=usercernersmartappoauth2';
+              break;
+            case 'f08c6f2c-1469-4805-98b0-0e2c14718fa1':
+              window.location = 'https://localhost:44379/account/login?returnUrl=http%3A%2F%2Flocalhost%3A3000%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=test1cernersmartappoauth2';
+              break;
+            case '4ed79774-438b-4c40-9a9d-7df67e2f92d0':
+              window.location = 'https://medicalcopilot.dev.lumedx.com/medicalcopilotidp/account/login?returnUrl=https%3A%2F%2Fmedicalcopilot.dev.lumedx.com%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=testcernersmartappoauth2';
+              break;
+            case 'ce726c87-2d1d-4824-9c4c-5e5598492a47':
+              window.location = 'https://idp-test.healthviewanalytics.com/account/login?returnUrl=https%3A%2F%2Fembeddedanalytics-test.healthviewanalytics.com%2F%23%2Fpatient%2F' + mrn + '%2Frisk&user=portal&provider=cernersmartappoauth2';
+              break;
+          }          
         });
       } else {
         onError();
